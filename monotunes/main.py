1becc8a64763c1da134bb4450d565e98d3d6fb9e
JamesSmith1202 <jsmith@stuy.edu>
2017-11-22 14:54:22 -0500

    Finished login and create flask pages

--- monotunes/main.py
+++ monotunes/main.py
@@ -1,17 +1,30 @@
 from flask import Flask, render_template, redirect, url_for, session, request, flash
 import os
-import api.py as api
-import db.py as db
+from utils import api, db
 
 USER_SESSION = "logged_in"
 
 app = Flask(__name__)
 app.secret_key = os.urandom(16)
 
+def is_null(username, password, confpw):
+    return username == "" or password == "" or confpw == ""
+
+def add_session(username, password):
+    if is_null(username, password, "filler"):
+            flash("Username or password is blank")
+            return False
+    if(db.login(username, password)):
+        session[USER_SESSION] = username
+        return True
+    else:
+        flash("Incorrect login credentials")
+        return False
+
 @app.route("/")
 def root():
-        return render_template("home.html", top_songs = api.get_top_songs(), in_session = USER_SESSION in session)
-    
+    return render_template("home.html", top_songs = api.get_top_songs(), in_session = USER_SESSION in session)
+
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if (request.method == "GET"):
@@ -19,12 +32,26 @@ def login():
     else:
         username = request.form["username"]
         password = request.form["password"]
-        if username == "" or password == "":
-            pass
+        if add_session(username, password):
+            return redirect(url_for("home"))
+        return render_template("login.html")     
 
 @app.route("/create", methods=["GET", "POST"])
 def create():
-    pass
+    if !request.method == "GET":
+        username = request.form["username"]
+        password = request.form["password"]
+        confirm_passsword = request.form["confirm_password"]
+        if is_null(username, password, confirm_password):
+            flash("A field was left empty")
+        elif password != confirm_password:
+            flash("Password and password confirmation do not match")
+        else:
+            if !create_account(username, password):
+                flash("Username taken")
+            else:
+                return redirect(url_for("login"))
+    return render_template("create.html")
 
 @app.route("/profile")
 def profile():
